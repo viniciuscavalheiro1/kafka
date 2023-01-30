d13fe5f84277560f1502beb66ddc1db37ea7b219
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2023-01-30 17:19:47 -0300

    add callback function

--- str-producer/HELP.md
+++ str-producer/HELP.md
@@ -26,3 +26,7 @@ The following guides illustrate how to use some features concretely:
 * [Serving Web Content with Spring MVC](https://spring.io/guides/gs/serving-web-content/)
 * [Building REST services with Spring](https://spring.io/guides/tutorials/rest/)
 
+
+## Build image Zookeeper
+
+
--- str-producer/src/main/java/com/vinicius/strproducer/StrProducerApplication.java
+++ str-producer/src/main/java/com/vinicius/strproducer/StrProducerApplication.java
@@ -2,8 +2,9 @@ package com.vinicius.strproducer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class StrProducerApplication {
 
     public static void main(String[] args) {
